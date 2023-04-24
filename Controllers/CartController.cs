6212cd306b1866ae0688fd0cdf04c95e989dae0d
Scott Winiarski <Scott1w1@hotmail.com>
2023-04-24 11:59:34 -0500

    Started on View, created Controller function, added modification to _layout

---
+++ Controllers/CartController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+public class CartController : Controller{
+    private DataContext _dataContext;
+    private UserManager<AppUser> _userManager;
+    public CartController(DataContext db, UserManager<AppUser> usrMgr)
+    {
+        _dataContext = db;
+        _userManager = usrMgr;
+    }
+    [Authorize(Roles = "northwind-customer")]
+    public IActionResult Index() {
+        int cid = _dataContext.Customers.First(c => c.Email == User.Identity.Name).CustomerId;
+        return View(_dataContext.CartItems.Where(c => c.CustomerId == cid));
+    }
+}
